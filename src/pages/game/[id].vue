771b2716b35b8d4637bdca45e8f07481b4615cef
jhygreatbug <jhygreatbug@gmail.com>
2024-06-22 00:20:50 +0800

    feat: 润起来了

---
+++ src/pages/game/[id].vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { getGameById } from '~/data/few-iframe-game'
+
+const route = useRoute('/game/[id]')
+
+const game = computed(() => getGameById(route.params.id))
+</script>
+
+<template>
+  <div>
+    <iframe
+      v-if="game?.iframeSrc" :src="game.iframeSrc" frameborder="0" :style="{
+        width: `${game.width}px`,
+        height: `${game.height}px`,
+      }"
+    />
+  </div>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,51 +1,24 @@
 <script setup lang="ts">
+import { list } from '~/data/few-iframe-game'
+
 defineOptions({
   name: 'IndexPage',
 })
-const user = useUserStore()
-const name = ref(user.savedName)
 
 const router = useRouter()
-function go() {
-  if (name.value)
-    router.push(`/hi/${encodeURIComponent(name.value)}`)
+function go(id: string) {
+  router.push(`/game/${id}`)
 }
-
-const { t } = useI18n()
 </script>
 
 <template>
-  <div>
-    <div text-4xl>
-      <div i-carbon-campsite inline-block />
+  <div v-for="game in list" :key="game.id" class="game" @click="go(game.id)">
+    <img :src="game.avatar" alt="">
+    <div class="game__title">
+      {{ game.title }}
     </div>
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank">
-        Vitesse
-      </a>
-    </p>
-    <p>
-      <em text-sm opacity-75>{{ t('intro.desc') }}</em>
-    </p>
-
-    <div py-4 />
-
-    <TheInput
-      v-model="name"
-      :placeholder="t('intro.whats-your-name')"
-      autocomplete="false"
-      @keydown.enter="go"
-    />
-    <label class="hidden" for="input">{{ t('intro.whats-your-name') }}</label>
-
-    <div>
-      <button
-        m-3 text-sm btn
-        :disabled="!name"
-        @click="go"
-      >
-        {{ t('button.go') }}
-      </button>
+    <div class="game__description">
+      {{ game.description }}
     </div>
   </div>
 </template>
